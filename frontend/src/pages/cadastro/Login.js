ced1d0f1ace2f7ad72680e69ec0f1dda3627d660
Julio Neves <julionev10@gmail.com>
2021-05-16 13:38:47 -0300

    Julio 16/05/2021 Adição da tela login

---
+++ frontend/src/pages/cadastro/Login.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entrar iService</title>
+    <link rel="stylesheet" href="syles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="logo">
+        <header>
+            <a href="../Home.html"><img src="../Assets/iservice_icon_blue.png" alt="" width="200"></a>
+        </header>
+    </div>
+    <!-----Corpo do login------>
+    <div class="mainContainer">
+        <section id="container">
+            <div>
+                <div class="card texto">
+                </div>
+                <h2 style="color: black;">Faça login para continuar</h2>
+                <div class="card">
+                    <div class="info1">
+                        <div class="info2">
+                            <div class="login">
+                                <input placeholder="Login" type="text">
+                            </div>
+                            <div class="senha">
+                                <input placeholder="Senha" type="password" size="10">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="entrarButton">Entrar</button>
+            <a href="">Esqueci a minha senha</a>
+            <a href="">Não tem conta? Cadastre-se</a>
+        </section>
+    </div>
+
+</body>
+
+</html>
