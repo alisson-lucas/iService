454db0bb354ce59c7783fad20724658596fc851d
alisson-lucas <lucasoliveira.alisson@gmail.com>
2021-05-31 18:34:04 -0300

    master | botao cliente/profissional funcionando

--- iservice/src/pages/UserSignIn/index.tsx
+++ iservice/src/pages/UserSignIn/index.tsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 import RNPickerSelector from 'react-native-picker-select';
 import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
@@ -7,10 +7,12 @@ import { Container, ScrollContainer, Text, TextInput, SelectView, FormButton, Te
 
 export default function UserSignIn(){
   const [type, setType] = useState('');
+  const [isProfessional, setIsProfessional] = useState(false);
+  const [radioValue, setRadioValue] = useState('');
 
-  var radio_props = [
-    {label: 'Cliente', value: 0 },
-    {label: 'Profissional', value: 1 }
+  var profissoes = [
+    {label: 'Cliente', value: 'Cliente' },
+    {label: 'Profissional', value: 'Profissional' }
   ];
 
   return (
@@ -18,41 +20,46 @@ export default function UserSignIn(){
       <Container>
           <Text>Insira seus dados para concluir o cadastro</Text>
           <RadioForm 
-          radio_props={radio_props}
+          radio_props={profissoes}
           initial={0}
           formHorizontal={true}
           buttonColor={'#000080'}
-          onPress={() => {
-            if(radio_props.length === 2) {
-
-              return (
-                <>
-                  <TextInput placeholder="Telefone" keyboardType="numeric"></TextInput>
-                  <SelectView>
-                  <RNPickerSelector
-      
-                      onValueChange={(value) => setType(value)}
-      
-                      items={[
-                      { label: 'Masculino', value: 'Masculino', color: '#000' },
-                      { label: 'Feminino', value: 'Feminino', color: '#000' },
-                      ]}
-      
-                      style={{ inputAndroid: { color: '#000' }, inputIOS: { color: '#000' } }}
-      
-                      placeholder={{ label: 'Gênero', value: ''}}
-                      />
-                  </SelectView>
-                </>
-              )
+          onPress={(value) => {
+              if(value === 'Profissional'){
+                setRadioValue(value)
+                setIsProfessional(true)
+              } else {
+                setRadioValue(value)
+                setIsProfessional(false)
+              }
             }
-          }}
+          }
           />
           <TextInput placeholder="Login"></TextInput>
           <TextInput placeholder="Senha"></TextInput>
           <TextInput placeholder="Repetir senha"></TextInput>
           <TextInput placeholder="Nome"></TextInput>
           <TextInput placeholder="Cpf"></TextInput>
+          {isProfessional ? 
+            <>
+              <TextInput placeholder="Telefone" keyboardType="numeric"></TextInput>
+              <SelectView>
+              <RNPickerSelector
+
+                  onValueChange={(value) => setType(value)}
+
+                  items={[
+                  { label: 'Masculino', value: 'Masculino', color: '#000' },
+                  { label: 'Feminino', value: 'Feminino', color: '#000' },
+                  ]}
+
+                  style={{ inputAndroid: { color: '#000' }, inputIOS: { color: '#000' } }}
+
+                  placeholder={{ label: 'Gênero', value: ''}}
+                  />
+              </SelectView>
+            </> : <></>
+          }
           
 
           <FormButton>
