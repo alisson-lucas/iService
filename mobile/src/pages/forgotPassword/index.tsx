b47edb33f4a9bd1057d30b20e54787abde267eb4
alisson-lucas <lucasoliveira.alisson@gmail.com>
2021-09-16 11:39:52 -0300

    forgotPassword | telas confirmaçao de email e nova senha

--- mobile/src/pages/forgotPassword/index.tsx
+++ mobile/src/pages/forgotPassword/index.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { AntDesign as Icon, FontAwesome5 } from '@expo/vector-icons'
 
-import { Container, Title, SubTitle, ConfirmButton, TextButton } from './styles';
+import { Container, HeaderContainer, BodyContainer, CardEmail, CardPassword, Title, SubTitle, ConfirmButton, TextButton } from './styles';
 
 import blueVersion from '../../styles/colors';
 
 const forgotPassword: React.FC = () => {
+  const [isValid, setIsValid] = useState<boolean>(false);
+
+  const navigation = useNavigation();
+
+  function handleBackScreen(){
+    navigation.goBack();
+  };
+
+  const confirmEmail = () => {
+    setIsValid(!isValid);
+  };
+
   return (
       <Container>
-          <Title>Esqueci a senha</Title>
-          <SubTitle>Digite o seu email:</SubTitle>
-          <TextInput style={styles.input}></TextInput>
-          <ConfirmButton>
-            <TextButton>Confirmar</TextButton>
-          </ConfirmButton>
+        <HeaderContainer>
+          <Icon name="close" color="#fff" size={25}/>
+          <Title>Recuperar senha</Title>
+          <Icon name="close" size={25} onPress={handleBackScreen}/>
+        </HeaderContainer>
+        <BodyContainer>
+          {!isValid ? 
+            <CardEmail>
+              <SubTitle>Digite o seu email:</SubTitle>
+              <TextInput style={styles.input}></TextInput>
+              <ConfirmButton onPress={confirmEmail}>
+                <TextButton>Confirmar</TextButton>
+              </ConfirmButton>
+            </CardEmail>
+
+            : <CardPassword>
+                <SubTitle>Digite a nova senha:</SubTitle>
+                <TextInput style={styles.input}></TextInput>
+                <SubTitle>Repita a nova senha:</SubTitle>
+                <TextInput style={styles.input}></TextInput>
+                <ConfirmButton>
+                  <TextButton>Confirmar</TextButton>
+                </ConfirmButton>
+            </CardPassword>
+          }
+        </BodyContainer>
       </Container>
   );
 }
--- mobile/src/pages/forgotPassword/styles.ts
+++ mobile/src/pages/forgotPassword/styles.ts
@@ -3,17 +3,62 @@ import styled from "styled-components/native";
 export const Container = styled.SafeAreaView`
     flex: 1;
     align-items: center;
+    justify-content: flex-start;
+    background-color: #fff;
+`;
+
+export const HeaderContainer = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: row;
+    width: 100%;
+    margin: 30px 0px 10px 0;
+`;
+
+export const BodyContainer = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    /* height: 200px; */
+    /* background-color: #008080; */
+`;
+
+export const CardEmail = styled.View`
+    display: flex;
+    align-items: center;
     justify-content: center;
     background-color: #fff;
+    height: 220px;
+    width: 80%;
+    border-radius: 15px;
+    box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.2);
+    margin-bottom: 50px;
+`;
+
+export const CardPassword = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    height: 270px;
+    width: 80%;
+    border-radius: 15px;
+    box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.2);
+    margin-bottom: 50px;
 `;
 
 export const Title = styled.Text`
     color: #000;
+    font-weight: bold;
+    font-size: 16px;
 `;
 
 export const SubTitle = styled.Text`
     color: #000;
     font-weight: bold;
+    margin-top: 15px;
 `;
 
 export const ConfirmButton = styled.TouchableOpacity`
